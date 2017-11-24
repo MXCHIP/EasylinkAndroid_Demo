34e7f13aec309efbba04d105572384db7bd7f19c
尤晓翔 <youxx@mxchip.com>
2017-11-24 14:18:46 +0800

    更新demo

--- app/src/main/java/io/fogcloud/demo/MainActivity.java
+++ app/src/main/java/io/fogcloud/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package io.fogcloud.demo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -35,64 +36,70 @@ public class MainActivity extends AppCompatActivity {
         final EasylinkP2P elp2p = new EasylinkP2P(mContext);
 
 
-        TextView easylinktest = (TextView) findViewById(R.id.easylinktest);
-        TextView easylinkstop = (TextView) findViewById(R.id.easylinkstop);
-
-        EditText et = (EditText) findViewById(R.id.ssid);
-        et.setText(el.getSSID());
+        final TextView easylinktest = (TextView) findViewById(R.id.easylinktest);
+//        TextView easylinkstop = (TextView) findViewById(R.id.easylinkstop);
 
         final EditText psw = (EditText) findViewById(R.id.psw);
         final EditText ssid = (EditText) findViewById(R.id.ssid);
+        if(ssid!=null) {
+            ssid.setText(el.getSSID());
+        }
 
         log_view = (EditText) findViewById(R.id.log);
-        log_view.setText("");
+        if(log_view != null) {
+            log_view.setText("");
+        }
 
-        easylinktest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EasyLinkParams elp = new EasyLinkParams();
-                elp.ssid = ssid.getText().toString().trim();
-                elp.password = psw.getText().toString().trim();
-                elp.sleeptime = 50;
-                elp.runSecond = 20000;
-                Toast.makeText(mContext, "open easylink", Toast.LENGTH_SHORT).show();
+        if(easylinktest != null & ssid != null & psw != null) {
+            easylinktest.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (easylinktest.getText().toString().equalsIgnoreCase("发送配网")) {
+                        easylinktest.setText("关闭配网");
+                        easylinktest.setBackgroundColor(Color.rgb(255, 0, 0));
+                        EasyLinkParams elp = new EasyLinkParams();
+                        elp.ssid = ssid.getText().toString().trim();
+                        elp.password = psw.getText().toString().trim();
+                        elp.sleeptime = 50;
+                        elp.runSecond = 20000;
+                        Toast.makeText(mContext, "open easylink", Toast.LENGTH_SHORT).show();
 
-//                el.startEasyLink(elp, new EasyLinkCallBack() {
-                elp2p.startEasyLink(elp, new EasyLinkCallBack() {
-                    @Override
-                    public void onSuccess(int code, String message) {
-                        Log.d(TAG, message);
-                        send2handler(1, message);
+                        elp2p.startEasyLink(elp, new EasyLinkCallBack() {
+                            @Override
+                            public void onSuccess(int code, String message) {
+//                                Log.d(TAG,">>>>>>>>>>");
+                                Log.d(TAG, message);
+                                send2handler(1, message);
+                            }
+
+                            @Override
+                            public void onFailure(int code, String message) {
+                                Log.d(TAG, message);
+                            }
+                        });
+                    } else {
+                        easylinktest.setText("发送配网");
+                        easylinktest.setBackgroundColor(Color.rgb(63, 81, 181));
+                        Toast.makeText(mContext, "stop easylink", Toast.LENGTH_SHORT).show();
+                        elp2p.stopEasyLink(new EasyLinkCallBack() {
+                            @Override
+                            public void onSuccess(int code, String message) {
+//                                Log.d(TAG,"++++++++++++++");
+                                Log.d(TAG, message);
+                                send2handler(2, message);
+                            }
+
+                            @Override
+                            public void onFailure(int code, String message) {
+                                Log.d(TAG, message);
+                            }
+                        });
                     }
-
-                    @Override
-                    public void onFailure(int code, String message) {
-                        Log.d(TAG, message);
-                    }
-                });
-            }
-        });
-
-        easylinkstop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(mContext, "stop easylink", Toast.LENGTH_SHORT).show();
-                elp2p.stopEasyLink(new EasyLinkCallBack() {
-                    @Override
-                    public void onSuccess(int code, String message) {
-                        Log.d(TAG, message);
-                        send2handler(2, message);
-                    }
-
-                    @Override
-                    public void onFailure(int code, String message) {
-                        Log.d(TAG, message);
-                    }
-                });
-            }
-        });
-
+                }
+            });
+        }
     }
+
     private void send2handler(int code, String message) {
         Message msg = new Message();
         msg.what = code;
@@ -103,14 +110,14 @@ public class MainActivity extends AppCompatActivity {
     Handler LHandler = new Handler() {
         public void handleMessage(android.os.Message msg) {
             if (msg.what == 1) {
-                if(countno >2){
+                if (countno > 2) {
                     log_view.setText("");
                     countno = 1;
                 }
                 log_view.append(msg.obj.toString().trim() + "\r\n");
-                countno ++;
+                countno++;
             }
-            if(msg.what == 2) {
+            if (msg.what == 2) {
                 log_view.setText("");
             }
         }
